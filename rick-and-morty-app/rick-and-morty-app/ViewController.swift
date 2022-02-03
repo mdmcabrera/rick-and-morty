088129369724f4b9f7192e40052d60c5838522f0
Mar Cabrera <mdmcabreracast@gmail.com>
2022-02-03 21:00:08 +0000

    Add basic table view layout with custom cell

--- rick-and-morty-app/rick-and-morty-app/ViewController.swift
+++ rick-and-morty-app/rick-and-morty-app/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CharacterCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+
+    //MARK: - Properties
+    var characters = ["Character 1", "Character 2", "Character 3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setTableView()
     }
 
+    private func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characterCell", for: indexPath) as! CharacterCell
+
+        let character = characters[indexPath.row]
+        cell.nameLabel.text = character
+
+        return cell
+    }
 }
 
