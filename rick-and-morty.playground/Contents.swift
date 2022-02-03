ece9c3842b4ebd0b756c3af491a94f4060639567
Mar Cabrera <mdmcabreracast@gmail.com>
2022-02-03 19:58:00 +0000

    Refactor model to fetch a list of characters instead of an individual one

--- rick-and-morty.playground/Contents.swift
+++ rick-and-morty.playground/Contents.swift
@@ -1,5 +1,18 @@
 import UIKit
 
+// Data to get paginated response
+struct ResponseInfo: Codable {
+    let info: Info
+    let results: [Character]
+}
+
+struct Info: Codable {
+    let count: Int
+    let pages: Int
+    let next: String?
+    let prev: String?
+}
+
 struct Character: Codable {
     let id: Int
     let name: String
@@ -25,28 +38,30 @@ struct Location: Codable {
     let url: String
 }
 
-let url = URL(string: "https://rickandmortyapi.com/api/character/2")
+let url = URL(string: "https://rickandmortyapi.com/api/character")
 
-func fetchCharacter() async -> Character? {
+func fetchCharacter() async -> [Character] {
     guard let url = url else {
-        return nil
+        return []
     }
     
     do {
         let (data, _) = try await URLSession.shared.data(from: url)
         
-        let character = try JSONDecoder().decode(Character.self, from: data)
+        let decodedData = try JSONDecoder().decode(ResponseInfo.self, from: data)
+        let characters = decodedData.results
         
-        return character
+        return characters
     } catch {
-        return nil
+        return []
     }
 }
 
     Task {
-        let character = await fetchCharacter()
-        
-        print(character?.name)
+        let characters = await fetchCharacter()
+        for character in characters {
+            print(character.name)
+        }
     }
 
 
